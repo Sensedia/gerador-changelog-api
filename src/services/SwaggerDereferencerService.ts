26bc7c985b37c1d6f8aa303cce920e7891efa7eb
cleiltonoliveira <clltnoliveira@gmail.com>
2022-08-12 16:29:06 -0300

    Improve dereferencer service

--- src/services/SwaggerDereferencerService.ts
+++ src/services/SwaggerDereferencerService.ts
@@ -3,6 +3,12 @@ import SwaggerParser from "@apidevtools/swagger-parser"
 export default class SwaggerDereferencerService {
 
     public static async dereference(spec: string): Promise<any> {
-        return SwaggerParser.dereference(spec)
+        let objJson = {}
+        try {
+            objJson = await SwaggerParser.dereference(spec)
+        } catch (error) {
+            console.error(error)
+        }
+        return objJson
     }
 }
