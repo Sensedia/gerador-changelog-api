cca766434af7c6647ceccd6779857f2c65f681ff
André Ferreira Trindade <andreferreiratrindade@gmail.com>
2024-01-18 08:31:43 -0300

    It was added configurations to cors

--- lambda_handler.ts
+++ lambda_handler.ts
@@ -11,20 +11,31 @@ export const handler = async (
   event: APIGatewayEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> => {
-  console.log(event);
-  event.body
+
+
   let changeLogGeneratorService = new ChangeLogGeneratorService();
   try{
     
   let result = await changeLogGeneratorService.GenerateChangeLogWithUrlYaml(JSON.parse(event.body || ""));
   return {
     statusCode: 200,
+    headers: getheards().headers,
+
     body: JSON.stringify(result),
   };
 }catch(error){
   return {
+    headers: getheards().headers,
     statusCode: 400,
     body: JSON.stringify(error),
   };
 }
 };
+
+function getheards(){
+  return { headers: {
+    "Access-Control-Allow-Headers" : "Content-Type",
+    "Access-Control-Allow-Origin": process.env.AWS_CORS || "",
+    "Access-Control-Allow-Methods": "POST"
+}}
+}
